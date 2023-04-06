a9c124dbe446f0f452eb2aa078096c1149da4857
Leng <Tanapon.sot@Gmail.com>
2023-04-06 17:39:15 +0700

    resolve conflict navbar to homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,10 @@
 import "./App.css";
-import { NavbarLogin, NavbarUser } from "./pages/Navbar";
-
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <div className="App bg-black h-screen">
-      <br />
-      <br />
-      <br />
-      <center>
-        <h1 className="text-3xl text-red font-sans">Hello world! ล้างแอร์</h1>
-        <br />
-        <br />
-        <button className="btn-primary">Button Primary</button>
-        <button className="btn-primary-large">Button Primary</button>
-        <br />
-        <br />
-        <button className="btn-secondary">Button Secondary</button>
-        <button className="btn-secondary-large">Button Secondary</button>
-        <br />
-        <br />
-        <button className="btn-ghost">Button Ghost</button>
-      </center>
-      <NavbarLogin />
-      <br />
-      <NavbarUser />
-      <br />
-      
+    <div className="App bg-BG h-screen">
+      <Homepage />
     </div>
   );
 }
