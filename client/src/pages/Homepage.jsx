8fc72b03442d504be0594b5b8edf3d5aea1e6b9f
Leng <Tanapon.sot@Gmail.com>
2023-04-06 17:46:42 +0700

    resolve conflict service to homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import { NavbarLogin } from "./navbar";
 import Header from "./Header";
+import Service from "./Service";
 
 const Homepage = () => {
   return (
     <>
       <NavbarLogin />
       <Header />
+      <Service />
     </>
   );
 };
