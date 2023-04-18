6f5dd9a5da9fc0e2890aa09d6ce4368d7c1b1833
Leng <Tanapon.sot@Gmail.com>
2023-04-18 13:29:36 +0700

    feat(servicepage):update content

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,18 +1,10 @@
 import Header from "./homepage/Header";
 import Service from "./homepage/Service";
 import FooterHomepage from "./homepage/FooterHomepage";
-import {
-  NavService,
-  HeaderService,
-  Footerservice,
-} from "./service/ServicesMore";
 
 const Homepage = () => {
   return (
     <>
-      <HeaderService />
-      <NavService />
-      <Footerservice />
       <Header />
       <Service />
       <FooterHomepage />
--- client/src/pages/ServicesPage.jsx
+++ client/src/pages/ServicesPage.jsx
@@ -1,1 +1,17 @@
-import { HeaderService } from "./service/ServicesMore";
+import {
+  HeaderService,
+  Footerservice,
+  NavService,
+} from "./service/ServicesMore";
+
+const ServicesPage = () => {
+  return (
+    <>
+      <HeaderService />
+      <NavService />
+      <Footerservice />
+    </>
+  );
+};
+
+export default ServicesPage;
