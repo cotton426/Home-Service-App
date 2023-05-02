c1581cca6219ad7602f3510613d36e0cfcd66f14
Leng <Tanapon.sot@Gmail.com>
2023-05-02 11:34:16 +0700

    feat(detail-information):update content

--- client/src/components/DetailInformation.jsx
+++ client/src/components/DetailInformation.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { TimePicker } from "antd";
 
 const DetailInformation = () => {
   const [inputValues, setInputValues] = useState({
     date: "",
-    time: "",
+    time: new Date().toLocaleTimeString("th-TH", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    }),
     address: "",
     subdistrict: "",
     district: "",
@@ -13,14 +18,9 @@ const DetailInformation = () => {
     note: "",
   });
 
-  const handleInputChange = (e) => {
-    setInputValues({
-      [e.target.name]: e.target.value,
-    });
-    console.log(setInputValues);
-  };
+  console.log(setInputValues);
 
-  const DetailForm = ({ label, id, name, type, placeholder, onChange }) => {
+  const DetailForm = ({ label, id, name, type, placeholder }) => {
     return (
       <div className="flex flex-col">
         <label htmlFor={id}>
@@ -33,12 +33,24 @@ const DetailInformation = () => {
           name={name}
           className="input-default"
           placeholder={placeholder}
-          onChange={onChange}
         />
         <ErrorMessage name={name} component="p" className="error-massage" />
       </div>
     );
   };
+
+  const today = new Date();
+  const tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+
+  const validationSchema = Yup.object().shape({
+    date: Yup.date().min(tomorrow).required("กรุณากรอกวันที่"),
+    time: Yup.string().required("กรุณากรอกเวลา"),
+    address: Yup.string().required("กรุณากรอกที่อยู่"),
+    subdistrict: Yup.string().required("กรุณากรอกแขวง/ตำบล"),
+    district: Yup.string().required("กรุณากรอกเขต/อำเภอ"),
+    province: Yup.string().required("กรุณากรอกจังหวัด"),
+  });
 
   const formFields = [
     {
@@ -51,7 +63,8 @@ const DetailInformation = () => {
       label: "เวลาที่สะดวกใช้บริการ",
       name: "time",
       type: "time",
-      placeholder: "กรุณาเลือกเวลา",
+      component: TimePicker,
+      placeholder: "กรุณากรอกเวลา",
     },
     {
       label: "ที่อยู่",
@@ -89,44 +102,68 @@ const DetailInformation = () => {
     <div className="w-3/5 flex box">
       <h1 className="w-[735px] h-[568px]  pt-6 pl-6">
         <p className="pb-9">กรอกข้อมูล</p>
-        <Formik
-          initialValues={inputValues}
-          validationSchema={Yup.object().shape({
-            date: Yup.date().min(new Date()).required("กรุณากรอกวันที่"),
-            time: Yup.string().required("กรุณากรอกเวลา"),
-            address: Yup.string().required("กรุณากรอกที่อยู่"),
-            subdistrict: Yup.string().required("กรุณากรอกแขวง/ตำบล"),
-            district: Yup.string().required("กรุณากรอกเขต/อำเภอ"),
-            province: Yup.string().required("กรุณากรอกจังหวัด"),
-          })}
-        >
+        <Formik initialValues={inputValues} validationSchema={validationSchema}>
           <Form className="grid grid-cols-2 gap-4 mt-8">
-            {formFields.map((field, handleChange) => (
+            {formFields.map((field, errors, touched) => (
               <div key={field.name} className="flex flex-col">
                 <label htmlFor={field.name}>
                   {field.label}
                   <label className="text-red">*</label>
                 </label>
-                <Field
-                  type={field.type}
-                  id={field.name}
-                  name={field.name}
-                  className="input-default"
-                  placeholder={field.placeholder}
-                  onChange={handleChange}
-                  {...(field.type === "date" && {
-                    min: new Date().toISOString().slice(0, 10),
-                    onFocus: (e) => (e.target.type = "date"),
-                    onBlur: (e) => (e.target.type = "text"),
-                  })}
-                  {...(field.type === "time" && {
-                    onFocus: (e) => (e.target.type = "time"),
-                    onBlur: (e) => (e.target.type = "text"),
-                    min: "00:00",
-                    max: "23:59",
-                    step: 60,
-                  })}
-                />
+                {field.component === TimePicker ? (
+                  <Field
+                    format="HH:mm"
+                    type="text"
+                    required
+                    component={TimePicker}
+                    name={field.name}
+                    className="box h-11"
+                    onChange={(time) => {
+                      setInputValues((prevValues) => ({
+                        ...prevValues,
+                        [field.name]: time ? time.format("HH:mm") : "",
+                      }));
+                    }}
+                    placeholder={field.placeholder}
+                    hideDisabledOptions={true}
+                    disabledHours={() => {
+                      const disabledHours = [];
+                      for (let i = 0; i < 9; i++) {
+                        disabledHours.push(i);
+                      }
+                      for (let i = 19; i < 24; i++) {
+                        disabledHours.push(i);
+                      }
+                      return disabledHours;
+                    }}
+                    disabledMinutes={(selectedHour) => {
+                      if (selectedHour === 18) {
+                        return Array.from({ length: 60 }).map((_, i) =>
+                          i < 0 || i > 0 ? i : null
+                        );
+                      }
+                    }}
+                  />
+                ) : (
+                  <Field
+                    type={field.type}
+                    id={field.name}
+                    name={field.name}
+                    className="input-default"
+                    placeholder={field.placeholder}
+                    {...(field.type === "date" && {
+                      type: "text",
+                      min: tomorrow.toISOString().slice(0, 10),
+                      onFocus: (e) => (e.target.type = "date"),
+                    })}
+                    // onChange={(e) => {
+                    //   setInputValues({
+                    //     ...values,
+                    //     [field.name]: e.target.value,
+                    //   });
+                    // }}
+                  />
+                )}
                 <ErrorMessage
                   name={field.name}
                   component="p"
@@ -138,6 +175,7 @@ const DetailInformation = () => {
         </Formik>
       </h1>
       <h2 className="w-[349px] h-[349px] mr-40 pt-4 pl-6">สรุปรายการ</h2>
+      <p></p>
     </div>
   );
 };
