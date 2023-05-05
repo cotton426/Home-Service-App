5819c4779d1f0dcc2d1fe1648ffc99630af8b007
Net <net@Nets-MacBook-Air.local>
2023-05-06 00:53:29 +0700

    feat(omise-payment): 90% remain just handle error and can do authorized validation now

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,12 +1,14 @@
-import { supabase } from "../utils/supabase";
+import { supabase } from "../utils/supabase.js";
 
-export const requireAuth = (req, res, next) => {
+export const requireAuth = async (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) return res.status(401).json({ error: "Unauthorized" });
-
-  const { error } = supabase.auth.api.getUser(token);
-
+  console.log(token);
+  const tok = token.split(" ")[1];
+  const { error, ...rest } = await supabase.auth.getUser(tok);
+  console.log(error);
+  // console.log(rest);
   if (error) return res.status(401).json({ error: error.message });
 
   next();
